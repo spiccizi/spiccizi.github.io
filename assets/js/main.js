ba53647a1813acbea61d198314e6f23416fc4642
Ivan Procaccini <ivanprocaccini905@gmail.com>
2023-12-03 15:21:04 +0000

    Rename specs to features and rm products section

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,6 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    // reset: true
 })
 
 sr.reveal(`.home__header, .section__title`,{delay: 600})
@@ -78,7 +77,7 @@ sr.reveal(`.home__footer`,{delay: 700})
 sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
 
 sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
-sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
-sr.reveal(`.specs__img, .discount__img`,{origin: 'right'})
+sr.reveal(`.features__data, .discount__animate`,{origin: 'left', interval: 100})
+sr.reveal(`.features__img, .discount__img`,{origin: 'right'})
 sr.reveal(`.case__img`,{origin: 'top'})
 sr.reveal(`.case__data`)
